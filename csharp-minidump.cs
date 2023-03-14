792dccacc452506111313120f009e5c3500cdfe0
Niklas265 <59866153+Niklas265@users.noreply.github.com>
2023-03-14 11:24:13 +0100

    added minidump-code

---
+++ csharp-minidump.cs
@@ -0,0 +1,192 @@
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace HelloWorld;
+
+public enum ProcessAccessFlags : uint
+{
+    All = 0x001F0FFF,
+    Terminate = 0x00000001,
+    CreateThread = 0x00000002,
+    VirtualMemoryOperation = 0x00000008,
+    VirtualMemoryRead = 0x00000010,
+    VirtualMemoryWrite = 0x00000020,
+    DuplicateHandle = 0x00000040,
+    CreateProcess = 0x000000080,
+    SetQuota = 0x00000100,
+    SetInformation = 0x00000200,
+    QueryInformation = 0x00000400,
+    QueryLimitedInformation = 0x00001000,
+    Synchronize = 0x00100000
+}
+
+public enum MINIDUMP_TYPE
+{
+    MiniDumpNormal = 0x00000000,
+    MiniDumpWithDataSegs = 0x00000001,
+    MiniDumpWithFullMemory = 0x00000002,
+    MiniDumpWithHandleData = 0x00000004,
+    MiniDumpFilterMemory = 0x00000008,
+    MiniDumpScanMemory = 0x00000010,
+    MiniDumpWithUnloadedModules = 0x00000020,
+    MiniDumpWithIndirectlyReferencedMemory = 0x00000040,
+    MiniDumpFilterModulePaths = 0x00000080,
+    MiniDumpWithProcessThreadData = 0x00000100,
+    MiniDumpWithPrivateReadWriteMemory = 0x00000200,
+    MiniDumpWithoutOptionalData = 0x00000400,
+    MiniDumpWithFullMemoryInfo = 0x00000800,
+    MiniDumpWithThreadInfo = 0x00001000,
+    MiniDumpWithCodeSegs = 0x00002000,
+    MiniDumpWithoutAuxiliaryState = 0x00004000,
+    MiniDumpWithFullAuxiliaryState = 0x00008000,
+    MiniDumpWithPrivateWriteCopyMemory = 0x00010000,
+    MiniDumpIgnoreInaccessibleMemory = 0x00020000,
+    MiniDumpWithTokenInformation = 0x00040000,
+    MiniDumpWithModuleHeaders = 0x00080000,
+    MiniDumpFilterTriage = 0x00100000,
+    MiniDumpValidTypeFlags = 0x001fffff
+}
+
+[StructLayout(LayoutKind.Sequential, Pack = 4)]
+public struct MINIDUMP_EXCEPTION_INFORMATION
+{
+    public uint ThreadId;
+    public IntPtr ExceptionPointers;
+    public int ClientPointers;
+}
+
+// Class that represents a Windows Process
+// Contains the handle, PID-Information, etc., as well as the functionality to dump the process through MinidumpWriteDump
+public class WindowsProcess
+{
+    [DllImport("dbghelp.dll", SetLastError = true)]
+    static extern bool MiniDumpWriteDump(
+    IntPtr hProcess,
+    UInt32 ProcessId,
+    SafeHandle hFile,
+    MINIDUMP_TYPE DumpType,
+    IntPtr ExceptionParam,
+    IntPtr UserStreamParam,
+    IntPtr CallbackParam);
+
+    [DllImport("kernel32.dll", SetLastError = true)]
+    public static extern IntPtr OpenProcess(
+      uint processAccess,
+      bool bInheritHandle,
+      uint processId
+    );
+    public static IntPtr OpenProcess(Process proc, uint flags)
+    {
+        return OpenProcess(flags, false, (uint)proc.Id);
+    }
+    private int pid;
+    private System.Diagnostics.Process proc;
+    private IntPtr handle;
+    private bool functional = true;
+    private const string errorString = "ERROR";
+   
+    public WindowsProcess(int pid)
+    {
+        this.pid = pid;
+
+        try
+        {
+            this.proc = System.Diagnostics.Process.GetProcessById(pid);
+            this.handle = OpenProcess(this.proc, (uint) ProcessAccessFlags.All);
+        }
+        catch (Exception e)
+        {
+            this.functional = false;
+        }
+
+    }
+
+    public int getPid()
+    {
+        return this.pid;
+    }
+
+    public string getProcessName()
+    {
+        if (this.functional)
+        {
+            return this.proc.ProcessName;
+        }
+        else
+        {
+            return errorString;
+        }
+    }
+
+    public IntPtr getHandle()
+    {
+        if (this.functional)
+        {
+            return this.handle;
+        }
+        else
+        {
+            return this.handle;
+        }
+    }
+
+    public bool getFunctionality()
+    {
+        return this.functional;
+    }
+
+    public bool dump(string path)
+    {
+        if(this.functional && this.handle.ToInt64() != 0)
+        {
+            FileStream file = File.Create(path);
+            MINIDUMP_EXCEPTION_INFORMATION info = new MINIDUMP_EXCEPTION_INFORMATION();
+            info.ClientPointers = 1;
+            info.ExceptionPointers = Marshal.GetExceptionPointers();
+            info.ThreadId = 0;
+            bool ret = MiniDumpWriteDump(this.handle, (uint)this.proc.Id, file.SafeFileHandle, MINIDUMP_TYPE.MiniDumpWithFullMemory, IntPtr.Zero, IntPtr.Zero, IntPtr.Zero);
+            file.Close();
+            return ret;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
+class Hello
+{
+
+    static void Main(string[] args)
+    {
+     
+        if(args.Length != 2)
+        {
+            System.Console.WriteLine("Usage: " + System.AppDomain.CurrentDomain.FriendlyName + " <pid> <DumpFile>");
+            return;
+        }
+
+        int pid = Int32.Parse(args[0]);
+
+        string path = args[1]; 
+        
+        WindowsProcess p = new WindowsProcess(pid);
+
+        if(p.getFunctionality())
+        {
+            System.Console.WriteLine("Attempting to dump process " + p.getProcessName() + " (PID: " + p.getPid() + ")...");
+            if(p.dump(path)) {
+                System.Console.WriteLine("Minidump created successfully!");
+            }
+            else
+            {
+                System.Console.WriteLine("ERROR: Minidump could not be created successfully!");
+            }
+        }
+        else
+        {
+            System.Console.WriteLine("ERROR: Please recheck the supplied PID");
+        }
+    }
+}
